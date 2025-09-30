c4ef318f73209aea7dbd6923e037781d27ccdd63
SriramBhagavan <srirambhagavan441@gmail.com>
2025-09-30 15:03:02 +0530

    added chat feature using websockets

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,15 +2,39 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { createSocketConnection } from '../utils/socket'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios'
 
 const Chat = () => {
 
     const {targetUserId}=useParams()
-    console.log(targetUserId)
+    
     const [messages,setMessages]=useState([])
     const [newMessage,setNewMessage]=useState("")
     const user=useSelector((store)=>store.user)
     const userId=user?._id
+
+    const fetchChatMessages=async()=>{
+        const chat=await axios.get(BASE_URL+"/chat/"+targetUserId,{withCredentials:true})
+        console.log(chat.data.messages)
+
+        const chatMessages=chat?.data?.messages.map((msg)=>{
+            const {senderId,text}=msg
+            return{
+                firstName:senderId?.firstName,
+                lastName:senderId?.lastName,
+                text 
+            }
+
+    })
+    setMessages(chatMessages)   
+}
+    useEffect(()=>{
+        fetchChatMessages()
+    },[])
+    
+    
+
     useEffect(()=>{
         if(!userId){
             return
@@ -19,9 +43,9 @@ const Chat = () => {
         socket.emit("joinChat",{
             firstName:user.firstName,
             userId,targetUserId})
-        socket.on("messageReceived",({firstName,text})=>{
+        socket.on("messageReceived",({firstName,lastName,text})=>{
     console.log(firstName+": "+text)
-    setMessages((messages)=>[...messages,{firstName,text}])
+    setMessages((messages)=>[...messages,{firstName,lastName,text}])
 })
 
         return ()=>{
@@ -29,10 +53,12 @@ const Chat = () => {
     }
     },[userId,targetUserId])
 
+
     const sendMessage=()=>{
         const socket=createSocketConnection()
         socket.emit("sendMessage",{
             firstName:user.firstName,
+            lastName:user.lastName,
             userId,
             targetUserId,
             text:newMessage
@@ -48,10 +74,10 @@ const Chat = () => {
         <div className='flex-1 overflow-scroll p-5'>
             {messages.map((msg,index)=>{
                 return(
-                    <div key={index} className="chat chat-start">
+                    <div key={index} className={"chat "+(user.firstName=== msg.firstName? "chat-end":"chat-start") }>
                     <div className="chat-header">
-                    {msg.firstName}
-                     <time className="text-xs opacity-50">2 hours ago</time>
+                    {`${msg.firstName} ${msg.lastName}`}
+                     <time className="text-xs opacity-50">1 min ago</time>
                     </div>
                      <div className="chat-bubble">{msg.text}</div>
                     <div className="chat-footer opacity-50">Seen</div>
