0054ac3fe2b8bda29115a9ed388da926280247be
SriramBhagavan <srirambhagavan441@gmail.com>
2025-10-01 15:14:57 +0530

    deploy

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -6,6 +6,6 @@
 // export const BASE_URL =
 //   process.env.REACT_APP_BACKEND_URL || "http://localhost:3000"; 
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+export const BASE_URL = "https://devtinder-backend-wy5a.onrender.com"
 
 
