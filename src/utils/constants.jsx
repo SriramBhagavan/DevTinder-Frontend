27b1cdd2757727a4491e798c6a78cecf59fe2ffa
SriramBhagavan <srirambhagavan441@gmail.com>
2025-10-01 15:09:56 +0530

    deploy

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -3,6 +3,9 @@
 //export const BASE_URL=location.hostname==="localhost"?"http://localhost:3000":"/api";
 // src/utils/constants.js
 // src/utils/constants.js
-export const BASE_URL =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:3000"; 
+// export const BASE_URL =
+//   process.env.REACT_APP_BACKEND_URL || "http://localhost:3000"; 
+
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+
 
